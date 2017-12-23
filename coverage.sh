bb0a756dbfefb837af45fb0a9b147edbce336006
Aldrin J D'Souza <aldrin@users.noreply.github.com>
2017-12-23 17:13:42 -0500

    Coverage tweaks
    - chore: Switch back to codecov.
    - chore: Cache kcov builds

--- coverage.sh
+++ coverage.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
 set -ex
 
-# Build kcov
-KCOV=34
-wget https://github.com/SimonKagstrom/kcov/archive/v$KCOV.tar.gz
-tar xfz v$KCOV.tar.gz
-cd kcov-$KCOV
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=$HOME ..
-make install
+# The coverage data directory
+declare -r coverage="$TRAVIS_BUILD_DIR/target/coverage"
+
+# If we don't have kcov already
+if [[ ! -f $HOME/bin/kcov ]]; then
+    # Download and build it
+    KCOV=34
+    wget https://github.com/SimonKagstrom/kcov/archive/v$KCOV.tar.gz
+    tar xfz v$KCOV.tar.gz
+    cd kcov-$KCOV
+    mkdir build
+    cd build
+    cmake -DCMAKE_INSTALL_PREFIX=$HOME ..
+    make install
+    cd $TRAVIS_BUILD_DIR
+fi
 
 # Run all tests - and measure coverage
 cargo test --no-run --message-format=json |
     jq -r 'select(.profile.test == true) | .filenames[]' |
-    xargs -n 1 $HOME/bin/kcov --coveralls-id=$COVERALLS --exclude-pattern=/.cargo,/usr/lib --verify $HOME/coverage
+    xargs -n 1 $HOME/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify $coverage
+
+# Publish reports to code-code
+bash <(curl -s https://codecov.io/bash)
